798acbfd06762b667c45cf8e8f2fb9edff056cb4
Husain Ali <husainali7865253@gmail.com>
2025-08-03 17:45:44 +0530

    update button

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,94 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [value, setValue] = useState('');
-  const [result, setResult] = useState('');
-
+  const [value, setValue] = useState("");
+  const [result, setResult] = useState("");
 
   const handleClick = (buttonValue) => {
     setValue((prev) => prev + buttonValue);
   };
   const handleCalculate = () => {
     try {
-      if (value === '') {
-        setResult('Error');
+      if (value === "") {
+        setResult("Error");
       } else {
         const evalResult = eval(value);
         setResult(evalResult);
       }
     } catch (err) {
-      setResult('Error');
+      setResult("Error");
       console.log(err);
     }
   };
   const handleClear = () => {
-    setValue('');
-    setResult('');
+    setValue("");
+    setResult("");
   };
 
   return (
-    <div className='container'>
+    <div className="container">
       <h1>React Calculator</h1>
 
       <input type="text" value={value} readOnly />
-      <p className='ans'>{result}</p>
+      <p className="ans">{result}</p>
 
       <div>
         <div>
-          <button className='btn'  onClick={() => handleClick('7')} >7</button>
-          <button  className='btn'  onClick={() => handleClick('8')} >8</button>
-          <button  className='btn'  onClick={() => handleClick('9')} >9</button>
-          <button  className='btn' onClick={() => handleClick('+')} >+</button>
+          <button className="btn" onClick={() => handleClick("7")}>
+            7
+          </button>
+          <button className="btn" onClick={() => handleClick("8")}>
+            8
+          </button>
+          <button className="btn" onClick={() => handleClick("9")}>
+            9
+          </button>
+          <button className="btn" onClick={() => handleClick("+")}>
+            +
+          </button>
         </div>
         <div>
-          <button  className='btn'  onClick={() => handleClick('4')} >4</button>
-          <button  className='btn'  onClick={() => handleClick('5')} >5</button>
-          <button  className='btn'  onClick={() => handleClick('6')} >6</button>
-          <button  className='btn'  onClick={() => handleClick('-')} >-</button>
+          <button className="btn" onClick={() => handleClick("4")}>
+            4
+          </button>
+          <button className="btn" onClick={() => handleClick("5")}>
+            5
+          </button>
+          <button className="btn" onClick={() => handleClick("6")}>
+            6
+          </button>
+          <button className="btn" onClick={() => handleClick("-")}>
+            -
+          </button>
         </div>
         <div>
-          <button  className='btn'  onClick={() => handleClick('1')} >1</button>
-          <button  className='btn'  onClick={() => handleClick('2')} >2</button>
-          <button  className='btn'  onClick={() => handleClick('3')} >3</button>
-          <button  className='btn'  onClick={() => handleClick('*')} >*</button>
+          <button className="btn" onClick={() => handleClick("1")}>
+            1
+          </button>
+          <button className="btn" onClick={() => handleClick("2")}>
+            2
+          </button>
+          <button className="btn" onClick={() => handleClick("3")}>
+            3
+          </button>
+          <button className="btn" onClick={() => handleClick("*")}>
+            *
+          </button>
         </div>
-          <button  className='btn'  onClick={handleClear} >c</button>
-         <button  className='btn'  onClick={() => handleClick('0')} >0</button>
-          <button  className='btn'  onClick={handleCalculate} >=</button>
-         <button  className='btn'  onClick={() => handleClick('/')} >/</button>
-        </div>
+        <button className="btn" onClick={handleClear}>
+          C
+        </button>
+        <button className="btn" onClick={() => handleClick("0")}>
+          0
+        </button>
+        <button className="btn" onClick={handleCalculate}>
+          =
+        </button>
+        <button className="btn" onClick={() => handleClick("/")}>
+          /
+        </button>
+      </div>
     </div>
   );
 }
